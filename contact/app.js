692d7da109e4d9b11ed0905be6305063beddd93b
vh8t <106828461+vh8t@users.noreply.github.com>
2024-02-18 16:02:44 +0100

    Add files via upload

---
+++ contact/app.js
@@ -0,0 +1,27 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email!');
+        return;
+    }
+
+    fetch('http://vht8.pythonanywhere.com/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            message: message
+        })
+    })
+
+    alert('Your message has been sent!');
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+});
