66502c78f85155d963508621787c0195757afe12
vh8t <106828461+vh8t@users.noreply.github.com>
2024-02-18 22:29:03 +0100

    Add files via upload

--- contact/app.js
+++ contact/app.js
@@ -1,3 +1,5 @@
+const header = document.getElementById("header");
+
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -25,3 +27,31 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     document.getElementById('email').value = '';
     document.getElementById('message').value = '';
 });
+
+function changeTheme(theme) {
+    const linkElement = document.getElementById("theme-css");
+    if (linkElement) {
+        linkElement.href = theme;
+        localStorage.setItem("theme", theme);
+    }
+}
+
+function loadTheme() {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+        changeTheme(theme);
+    } else {
+        changeTheme('theme-dark.css');
+    }
+}
+
+header.addEventListener("click", () => {
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "theme-dark.css") {
+        changeTheme("theme-light.css");
+    } else {
+        changeTheme("theme-dark.css");
+    }
+});
+
+window.onload = loadTheme;
