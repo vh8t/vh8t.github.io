88e5808fa88b7e8a57a10e9d89bcc5b1196b9ed5
Samuel Golis <golis.sam@gmail.com>
2025-10-25 14:03:43 +0200

    changed favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import '$lib/stores/theme';
-	import favicon from '$lib/assets/favicon.ico';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="https://vh8t.xyz/favicon.jpg" />
 </svelte:head>
 
 {@render children?.()}
