353c15ec7595bc6b2213ed64abf4ec6680b00a31
Samuel Golis <golis.sam@gmail.com>
2025-11-25 08:19:44 +0100

    changed theme and added stuff

--- src/routes/stuff/+page.svelte
+++ src/routes/stuff/+page.svelte
@@ -7,6 +7,14 @@
 
 	let timer: ReturnType<typeof setTimeout> | null = null;
 
+	// std::vector<std::pair<std::string, std::optional<std::string>>>
+	let stuff: [string, string | undefined][] = [
+		[
+			'AJ Prezentace',
+			'https://docs.google.com/presentation/d/1jDIsVeKMQFI2yHZraYj1n7pwvPNP96t_EIwoiNrW0VM/edit?usp=sharing'
+		]
+	];
+
 	const handleInteraction = () => {
 		if (timer) {
 			clearTimeout(timer);
@@ -25,8 +33,20 @@
 	<SiteHeader />
 
 	{#if show}
-		<div class="flex flex-1 flex-col p-4">
-			<p class="text-xl font-medium">Hello</p>
+		<div class="flex flex-1 flex-col gap-2 px-8 py-4">
+			{#each stuff as [text, url]}
+				{#if url}
+					<a
+						href={url}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="w-fit font-medium text-blue-600 transition-colors hover:text-blue-800 hover:underline"
+						>{text}</a
+					>
+				{:else}
+					<p class="text-gray-700">{text}</p>
+				{/if}
+			{/each}
 		</div>
 	{:else}
 		<div class="flex flex-1 flex-col items-center justify-center">
