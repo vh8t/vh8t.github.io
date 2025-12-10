037dcf84e47d426d8504fef7c70668a5b3349771
Samuel Golis <golis.sam@gmail.com>
2025-12-10 18:53:49 +0100

    added new levels

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,11 @@
 export type BlockType = 'hero' | 'vertical' | 'horizontal' | 'soldier';
 
 export interface Block {
-	id: string;
 	type: BlockType;
 	x: number;
 	y: number;
 	w: number;
 	h: number;
-	label?: string;
 }
 
 export interface GameState {
